cbe2c2490acabdfd0c8aab2b5050f9af2403343c
pbhatt2026 <pranavbhatt77@gmail.com>
2026-04-21 16:19:58 -0500

    Add configuration constants for trading parameters

---
+++ config.py
@@ -0,0 +1,11 @@
+MIN_PROB = 65
+MIN_CREDIT = 100
+MAX_TICKERS = 4
+
+CACHE_TTL = 300
+
+TAKE_PROFIT = 0.5     # 50% of credit
+STOP_LOSS = 1.5       # 150% of credit
+EXIT_DTE = 2          # close 2 days before expiry
+
+TD_API_KEY = "YOUR_TD_API_KEY"
